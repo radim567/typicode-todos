93c88dc8f76331e4a93807715397a8ec006ee2a4
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-04-15 23:50:10 +0200

    progressing...

--- typicode-todos/script.js
+++ typicode-todos/script.js
@@ -1,7 +1,7 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 const getTodos = () => {
-  fetch(apiUrl + '?_limit=5')
+  fetch(apiUrl + '?_limit=10')
     .then((res) => res.json())
     .then((data) => {
       data.forEach((todo) => addTodoToDOM(todo));
@@ -10,6 +10,7 @@ const getTodos = () => {
 
 const addTodoToDOM = (todo) => {
   const div = document.createElement('div');
+  div.classList.add('todo');
   div.appendChild(document.createTextNode(todo.title));
   div.setAttribute('data-id', todo.id);
 
@@ -32,13 +33,28 @@ const createTodo = (e) => {
     method: 'POST',
     body: JSON.stringify(newTodo),
     headers: { 'Content-type': 'application/json' },
-  }).then((res) => res.json())
-      .then((data) => addTodoToDOM(data));
+  })
+    .then((res) => res.json())
+    .then((data) => addTodoToDOM(data));
 };
+
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains('todo')) {
+    e.target.classList.toggle('done');
+  }
+
+  console.log(e.target.dataset.id);
+  // updateTodo(`Id is ${e.target.dataset.id}`);
+};
+
+const updateTodo = (id, completed) => {};
 
 const init = () => {
   document.addEventListener('DOMContentLoaded', getTodos);
   document.querySelector('#todo-form').addEventListener('submit', createTodo);
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleCompleted);
 };
 
 init();
